e297401f3c9f6ea356ac7d38d66c24845229d734
Your Name <sumisastri@gmail.com>
2019-10-17 20:21:49 +0100

    adds styling

--- src/front-end/common/nav.js
+++ src/front-end/common/nav.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const NavComponent = () => {
 	return (
-		<nav className="nav">
-			<p>Sign In/ Register</p>
-			<p>Sign Out</p>
+		<nav
+			className="nav"
+			style={{
+				display: 'flex',
+				justifyContent: 'flex-end'
+			}}
+		>
+			<p className="f3 link dim red underline pa3 pointer">Sign In</p>
+			<hr />
+			<p className="f3 link dim red underline pa3 pointer">Register</p>
+			<p className="f3 link dim red underline pa3 pointer">Sign Out</p>
 		</nav>
 	);
 };
