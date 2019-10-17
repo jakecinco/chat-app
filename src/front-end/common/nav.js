c6798c19abc7b97963ffc93641413d06afd3465e
Your Name <sumisastri@gmail.com>
2019-10-17 20:08:54 +0100

    adds functional nav-bar component

---
+++ src/front-end/common/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavComponent = () => {
+	return (
+		<nav className="nav">
+			<p>Sign In/ Register</p>
+			<p>Sign Out</p>
+		</nav>
+	);
+};
+
+export default NavComponent;
