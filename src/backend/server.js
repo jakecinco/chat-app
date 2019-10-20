a0ba2e1a3c9e00fbaa0f8776c9b1fb060f99fb80
Your Name <sumisastri@gmail.com>
2019-10-20 22:32:23 +0100

    adds npm run all

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// const getUserData = require('../api-chatData/userData.js');
+// app.get('/api-chatData/sign-in', (req, res) => res.send(getUserData()));
+
 app.listen(port, () => console.log(`chat-app listening on ${port}`));
