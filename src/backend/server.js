dbe2e4c806580ea346ca1e48fdb3245529892033
Your Name <sumisastri@gmail.com>
2019-10-20 23:50:01 +0100

    signin route working on insomnia

--- src/backend/server.js
+++ src/backend/server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
 	res.send('chat app is working');
 });
 
+app.post('/signin', (req, res) => {
+	res.json('this is the chat app sign in route');
+});
+
 app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname, '../../build', 'index.html'));
